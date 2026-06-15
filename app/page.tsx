a82eaf60e275b0ae274c96c5068ac166ef41b90b
fungaocun-stack <fungaocun@gmail.com>
2026-06-15 17:09:09 +0800

    升级到可视化CMS3

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { HomeFallback } from "@/components/home-fallback";
 import { VisualPageRender } from "@/components/visual-page-render";
 import { getPage, getProducts, getProjects, getSettings } from "@/lib/data";
+import { hasRenderableVisualPage } from "@/lib/visual-pages";
 
 export default async function Home() {
   const [page, products, projects, settings] = await Promise.all([getPage("home"), getProducts(), getProjects(), getSettings()]);
 
-  if (page?.content) {
+  if (hasRenderableVisualPage(page)) {
     return <VisualPageRender page={page} catalog={{ products, projects }} />;
   }
 
